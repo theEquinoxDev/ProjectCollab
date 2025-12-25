97a1b90dd90ecd43006ad5ad086280ff4b75db8a
Aditya <adityasuri11ss@gmail.com>
2025-12-25 15:27:47 +0530

    init

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import { app } from "./app";
+import dotenv from "dotenv";
+import connectDB from "../config/db";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
