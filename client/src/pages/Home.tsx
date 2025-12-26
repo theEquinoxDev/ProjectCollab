e634c6800ea8a542212b71219ec78f7fd16a353b
Aditya <adityasuri11ss@gmail.com>
2025-12-26 14:24:00 +0530

    removed unused import as this was failing deployment

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import ProblemSection from "../components/ui/ProblemSection";
 import HowItWorks from "../components/ui/HowItWorks";
